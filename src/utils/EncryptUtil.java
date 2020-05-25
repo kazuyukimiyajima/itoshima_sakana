ab4d77c2545ff5d818bfa1cb2f4d7cfb86fe9c3a
kazuyuki.miyajima <miya0302.1015@gmail.com>
2020-05-25 21:42:47 +0900

    Add password encrypt

---
+++ src/utils/EncryptUtil.java
@@ -0,0 +1,23 @@
+package utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.xml.bind.DatatypeConverter;
+
+public class EncryptUtil {
+    public static String getPasswordEncrypt(String plain_p, String salt) {//引数で受け取った文字列にソルト文字を連結させたものをSHA256で暗号化
+        String ret = "";
+
+        if(plain_p != null && !plain_p.equals("")) {
+            byte[] bytes;
+            String password = plain_p + salt;
+            try {
+                bytes = MessageDigest.getInstance("SHA-256").digest(password.getBytes());
+                ret = DatatypeConverter.printHexBinary(bytes);
+            } catch(NoSuchAlgorithmException ex) {}
+        }
+
+        return ret;
+    }
+}
